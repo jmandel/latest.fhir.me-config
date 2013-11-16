7c245ae232b7ed1c26324cfc509fc75455455b47
Josh Mandel <joshua.mandel@childrens.harvard.edu>
2013-11-16 21:57:05 +0000

    Space out double restart of fhir servers

--- fhir-deploy/update-fhir.sh
+++ fhir-deploy/update-fhir.sh
@@ -8,5 +8,13 @@ git pull
 
 JAVA_HOME=/usr/lib/jvm/java-7-oracle/ \
 /home/fhir/grails-2.3.2/bin/grails prod war
-sudo service fhir restart
-sudo service fhir-auth restart
+
+# Restart seems to end in 'no bean named transactionManager' ...
+sudo service fhir stop
+sleep 1
+sudo service fhir-auth start
+
+sleep 80
+sudo service fhir-auth stop
+sleep 1
+sudo service fhir start
